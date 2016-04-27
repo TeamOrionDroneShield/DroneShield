02daadbf2c6f26a87d9e7712dbeb2f1fae92f5fa
MasterChief93 <fede93.vagnoni@gmail.com>
2016-04-27 11:21:58 +0200

    Fix WeatherForcast: it will return just the currently weather condition

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -5,4 +5,10 @@ import requests
 
 def forecast_retrieval(latitude,longitude):
     r = requests.get('https://api.forecast.io/forecast/d968c76b162ac59de4c57cdbdcfbe83b/' + latitude + ',' + longitude)
-    return r.text
+    json_result = json.loads(r.text)
+    json_final = dict()
+    data = []
+    json_final['currently'] = data
+    data.append(json_result['currently'])
+    return str(json.dumps(json_final))
+
